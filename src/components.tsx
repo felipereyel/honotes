7703cf968daa19231e2535c331db55fa61cfd23c
Felipe Reyel <felipe.reyel@gmail.com>
2024-07-21 17:39:16 -0300

    Rename

--- src/components.tsx
+++ src/components.tsx
@@ -3,7 +3,7 @@ import { type Body } from './schema';
 const page = (title: string, Child: JSX.Element) => (
   <html>
     <head>
-      <title>{title} | Khono</title>
+      <title>{title} | Honotes</title>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <script src="https://cdn.tailwindcss.com"></script>
